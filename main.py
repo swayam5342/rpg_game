06c85ff715f70201cd12c7b2aecc3172f02d4dde
swayam2307 <swayamsb50@gmail.com>
2024-04-11 23:04:28 +0530

    modifi

--- main.py
+++ main.py
@@ -13,19 +13,19 @@ enemy = Enemy(name="Enemy", health=100, weapon=Crossbow)
 def main():
     with open('data.txt', '+a') as f:
         while True:
-            #os.system("cls")
+            os.system("cls")
             hero.attack(enemy)
             enemy.attack(hero)
             print(hero.health, enemy.health)
             f.write(f'{hero.health}  {enemy.health} \n')
             if enemy.health == 0:
                 print("You  Won")
-                #input(">")
+                input(">")
                 f.write("round end you won \n")
                 break
             if hero.health == 0:
                 print("You  Lost")
-                #input(">")
+                input(">")
                 f.write("round end you lost \n ")
                 break
 
